7df9becd93414597a660d5ea7be489c3e2a7c26d
SanderBuruma <sanderburuma@gmail.com>
2018-11-22 16:12:29 +0100

    nav fix

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<img src="{{ asset("svg/cubit32.svg") }}" height="32" alt="blue cube svg icon" /><a class="navbar-brand" href="<{{ route('home') }}">Default Laravel</a>
+	<img src="{{ asset("svg/cubit32.svg") }}" height="32" alt="blue cube svg icon" /><a class="navbar-brand" href="{{ route('home') }}">Default Laravel</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
