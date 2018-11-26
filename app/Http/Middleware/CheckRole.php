ce1447f49bf2cddb9d5302850064ccd659a5f618
SanderBuruma <sanderburuma@gmail.com>
2018-11-26 22:28:20 +0100

    added add statement to role check

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -21,17 +21,12 @@ class CheckRole
     {
         // | in checkrole parameter equals an OR statement. 2|3 means check whether user has role 2 OR 3.
         // ! before a role id means NOT. !2 passes if user does NOT have role 2.
-        // listed logical statements can be combined without issue. !2|1 passes if user has NOT role 2 OR has role 1. 
+        // & is an AND operator
+        // listed logical statements can be combined without issue as long as ! prefixes numbers and &| are inbetween role numbers. !2|1 passes if user has NOT role 2 OR has role 1.  2&!3 passes if user is not role 3 and has role 2.
 
         $saveCheckString = $checkRole."";
 
-        if (strpos($checkRole, "|") !== false) {
-            $checkRole = explode("|", $checkRole);
-        }
-
-        if (gettype($checkRole) == "string") {
-            $checkRole = [$checkRole];
-        };
+        $checkRole = explode("|", $checkRole);
 
         function isRole($request, $roleId) {
             foreach($request->user()->roles as $role){
@@ -43,20 +38,28 @@ class CheckRole
         };
 
         foreach ($checkRole as $roleId) {
-            if ($roleId[0] === "!") {
-                if (isRole($request, substr($roleId,1)) === false) {
-                    return $next($request);
+            $andStatement = true;
+            foreach(explode("&", $roleId) as $i){
+                if ($i[0] === "!") {
+                    if (isRole($request, substr($i,1)) === true) {
+                        $andStatement = false; break;
+                    }
+                } else {
+                    if (isRole($request, $i) === false) {
+                        $andStatement = false; break;
+                    }
                 }
-            } else {
-                if (isRole($request, $roleId)) {
-                    return $next($request);
-                }
+            }
+            if ($andStatement === true) {
+                return $next($request);
             }
         }
 
+        
+
         //error reporting
-        $saveCheckString = preg_replace(['/\|/','/\!/','/\d+/'], [" OR "," not:", "role-$0"], $saveCheckString);
-        Session::flash('error', "$saveCheckString role check failed, access denied.");
+        $saveCheckString = preg_replace(['/\|/', '/\!/', '/\&/'], [" OR "," not:", " AND "], $saveCheckString);
+        Session::flash('error', "role check failed, access denied. role $saveCheckString required");
         return redirect('/home');
 
     }
