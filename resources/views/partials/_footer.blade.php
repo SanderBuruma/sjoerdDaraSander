57d9265d0775fd93ecc5cff4185bbfa10adb6e4f
SanderBuruma <sanderburuma@gmail.com>
2018-11-26 09:14:45 +0100

    include hotjar

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -17,6 +17,18 @@
 {{-- select2 CDN --}}
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
 
+{{-- hotjar --}}
+<script>
+	(function(h,o,t,j,a,r){
+			h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+			h._hjSettings={hjid:1103743,hjsv:6};
+			a=o.getElementsByTagName('head')[0];
+			r=o.createElement('script');r.async=1;
+			r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+			a.appendChild(r);
+	})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
 @yield('footer')
 </body>
 </html>
