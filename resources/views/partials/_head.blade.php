bf8333b3d29948702ce7ad049c10f23d2b8f8383
SanderBuruma <sanderburuma@gmail.com>
2018-11-28 09:57:53 +0100

    changed name

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -22,5 +22,5 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 @yield('header')
-<title>Nervapolis @yield('title')</title>
+<title>Nervamarkt @yield('title')</title>
 </head>
