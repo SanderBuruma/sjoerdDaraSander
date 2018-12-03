7db0caec124d23daecadd43d5a566220fa160ebf
Sjoerd Veenstra <s.veenstra89@gmail.com>
2018-12-03 16:25:51 +0100

    composers toegevoegd

--- config/app.php
+++ config/app.php
@@ -173,6 +173,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        \Torann\GenIP\GeoIPServiceProvider::class,
 
     ],
 
@@ -224,7 +225,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => 'Collective\Html\FormFacade',
         'Html' => 'Collective\Html\HtmlFacade',
-
+        'GeoIP' => \Torann\GeoIP\Facades\GeoIP::class,
     ],
 
 ];
