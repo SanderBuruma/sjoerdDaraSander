24253a521b2689dea6d49184aa105709b25c7518
Sjoerd Veenstra <s.veenstra89@gmail.com>
2018-12-05 15:52:53 +0100

    geolocate view toegevoegd en weer verwijderd.

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -74,10 +74,10 @@
 		width: auto;
 	}
 
-	.map-container{
+	/* .map-container{
 		height: 400px;
 		width: 100%;
-	}
+	} */
 </style>
 @endsection
 
@@ -138,11 +138,11 @@
 
 		{{-- begin Map Marker ------------------------------------------------------------------------------------------------------------------------------ --}}
 		
-			<div class="map-container">
+			{{-- <div class="map-container">
 					<h3>My Google Maps Demo</h3>
 					<!--The div element for the map -->
 					<div id="map"></div>
-			</div>
+			</div> --}}
 
 		{{-- eind Map Marker ------------------------------------------------------------------------------------------------------------------------------ --}}
 			
@@ -197,26 +197,6 @@
 
 
 @section('footer')
-<script>
-		// begin map marker
-
-
-			// Initialize and add the map
-			function initMap() {
-			// The location of Uluru
-			var uluru = {lat: -25.344, lng: 131.036};
-			// The map, centered at Uluru
-			var map = new google.maps.Map(
-			document.getElementById('map'), {zoom: 4, center: uluru});
-			// The marker, positioned at Uluru
-			var marker = new google.maps.Marker({position: uluru, map: map});
-			}
-			</script>
-
-			 <script async defer
-    				src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBfM9rq072pO3kYg5hTX_69uA-6LeVKhF8&callback=initMap">
-			</script>
-		{{-- // eind map marker --}}
 
 <script>
 	var x = document.getElementById("latitude");
