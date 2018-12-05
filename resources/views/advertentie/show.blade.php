29a2fefa930848f22cb7b292fe5050be99c26a7d
SanderBuruma <sanderburuma@gmail.com>
2018-12-05 13:28:33 +0100

    advertentie show carousal added but incompletely

--- resources/views/advertentie/show.blade.php
+++ resources/views/advertentie/show.blade.php
@@ -4,17 +4,43 @@
 
 
 @section('header')
-
+<link rel="stylesheet" href="{{ asset('css/showadvertentie.css') }}">
 @stop
 
 
 @section('content')
-<div class="row"><div class="row-md-10 offset-md-1"><div class="card">
+<div class="row"><div class="row-md-10 offset-md-1 main"><div class="card">
 	<div class="card-header">
-
+		<h1>{{$advertentie->title}}</h1>
 	</div>
 	<div class="card-body">
-		
+		<p>{{$advertentie->description}}</p>
+	</div>
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img class="d-block w-100" src="/images/154400762373073291962.png" alt="First slide">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="/images/154400762373073291962.png" alt="Second slide">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="/images/154400762373073291962.png" alt="Third slide">
+			</div>
+		</div>
+		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
 	</div>
 </div></div></div>
 @stop
