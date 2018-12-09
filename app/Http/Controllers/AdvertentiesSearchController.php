b3271f9a9dae04d2ac12c5da01cae0fb09dacd13
SanderBuruma <sanderburuma@gmail.com>
2018-12-09 22:25:34 +0100

    search query basically works and displays advertenties as intended

--- app/Http/Controllers/AdvertentiesSearchController.php
+++ app/Http/Controllers/AdvertentiesSearchController.php
@@ -9,18 +9,18 @@ class AdvertentiesSearchController extends Controller
 {
 	public function homeSearch(Request $request) {
 		$queryWhereArr = [
-			['advertenties.title','like',"%$request->home_search_text%"],
+			['advertenties.title','like',"%$request->search_text%"],
 		];
-		if ($request->home_search_select != 1) {
-			//doesn't include this where condition if ALL is selected
-			$queryWhereArr[] = ['subcategories.category_id','=',$request->home_search_select];
+		if ($request->search_select != 1) {
+			//doesn't include this where condition if ALL category is selected
+			$queryWhereArr[] = ['subcategories.category_id','=',$request->search_select];
 		}
 		$advertenties = Advertentie::
-				where('price', '<', 500)
+				where('price', '<', 1e9)
 			->join('subcategories', 'advertenties.subcategory_id', '=', 'subcategories.id')
 			->select('advertenties.*', 'subcategories.category_id')
 			->where($queryWhereArr)
-			->limit(25)
+			->limit(12)
 			->orderByDesc('advertenties.created_at')
 			->get();
 		return $advertenties;
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,20 +7,21 @@
 @stop
 
 @section('content')
-<div class="row"><div class="col-md-10 offset-md-1 main">
+<div class= " row "><div class="col-md-10 offset-md-1 main">
 	<div id="search-bar" class="justify-content-center"><h4>
-		{!! Form::open(['route' => 'advertenties.search.index', 'method' => 'POST']) !!}
+		{{-- {!! Form::open(['route' => 'advertenties.search.index', 'method' => 'POST']) !!} --}}
 			<input name="home_search_text" id="home-search-text" type="text" placeholder="Zoekterm">
 			<select name="home_search_select" id="home-search-select">
 				@foreach($categories as $category)
 					<option value="{{$category->id}}">{{$category->name}}</option>
 				@endforeach
 			</select>
-		{!! Form::close() !!}
+			<button id="home-search-button">Zoek!</button>
+		{{-- {!! Form::close() !!} --}}
 	</h4></div>
 </div></div>
 
-<div class="row"><div class="col-md-10 offset-md-1 results"><div class="inside">
+<div class= " row "><div class="col-md-12 results"><div class="inside flex-row">
 
 </div></div></div>
 @endsection
@@ -35,33 +36,57 @@ jQuery(document).ready(function(){
 		}
 	});
 
-	$('.inside')[0].onkeydown = function(e){
-		console.log('submit');
+	$('#home-search-button')[0].onclick = searchQuery;
+	$('#home-search-text')[0].onkeydown = function(e) {
 		if (e.keyCode == 13){
-			console.log('keycode'+e.keyCode);
 			e.preventDefault();
-
-
-			jQuery.ajax({
-				url: "/item",
-				method: 'post',
-				data: {
-					wish: jQuery('#new-wish').val(),
-					user_id: jQuery('#user-id').val()
-				},
-				success: function(result){
-					console.log('ajax success');
-					$('#new-wish')[0].value = '';
-					console.log(result);
-					refreshWishList();
-				},
-				error: function(jqxhr, status, exception) {
-					console.log('Exception:', exception);
-					console.log(status);
-				}
-			});
-		};
+			searchQuery();
+		}
 	};
 });
+
+function searchQuery(){
+	jQuery.ajax({
+		url: "/advertenties.search.index",
+		method: 'post',
+		data: {
+			search_text: jQuery('#home-search-text').val(),
+			search_select: jQuery('#home-search-select').val()
+		},
+		success: function(result){
+			console.log(result);
+			refreshResults(result);
+		},
+		error: function(jqxhr, status, exception) {
+			console.log('Exception:', exception);
+			console.log(status);
+		}
+	});
+};
+
+function refreshResults(searchResults){
+	searchResultList = $('.inside')[0];
+	insideStr = '';
+	let count = 1;
+	for (let i of searchResults){
+		let createdAtSplit = i.created_at.split(/[\D]/);
+		console.log(createdAtSplit);
+		let date = parseInt(createdAtSplit[2])+
+			[ " Jan ", " Feb ", " Mar ", " Apr ", " Mei ", " Jun ", " Jul ", " Aug ", " Sep ", " Oct ", " Nov ", " Dec "][createdAtSplit[1]-1]+
+			createdAtSplit[0]+" - "+
+			createdAtSplit[3]+":"+createdAtSplit[4];
+
+		insideStr += `
+			<div class="advertentie"><a href="/advertentie/${i.slug}" target="_blank" rel="noopener noreferrer">
+				<h6 class="title" style="font-size: 2rem;">${i.title.substr(1, 25)}</h6>
+				<p class="price" style="font-size: 1.4rem;">€${i.price/100}</p>
+				<p class="date">${date}</p>
+				<img src="/images/${i.photo1||"empty-box.jpeg"}" width= "380" height= "300">
+				<p class="description">${i.description.substr(1, 100)}</p>
+			</a></div>
+		`;
+	};
+	searchResultList.innerHTML = insideStr;
+};
 </script>
 @stop
