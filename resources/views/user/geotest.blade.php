1d6118b49dc8b54d9593442e1177e3a5d84c40e0
dara <dara008@hotmail.com>
2018-12-11 10:24:55 +0100

    locakiser

--- resources/views/user/geotest.blade.php
+++ resources/views/user/geotest.blade.php
@@ -1,9 +1,8 @@
-<!DOCTYPE html>
 <html>
   <head>
-    <title>Place Searches</title>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
+    <title>Marker Clustering</title>
     <style>
       /* Always set the map height explicitly to define the size of the div
        * element that contains the map. */
@@ -19,72 +18,20 @@
     </style>
   </head>
   <body>
-
     <div id="map"></div>
-
-        
-
-    
     <script>
-        // This example requires the Places library. Include the libraries=places
-        // parameter when you first load the API. For example:
-        // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-  
-  let users = <?php echo json_encode($users) ?>;
-  console.log(users);
-  
-  function initMap() {
-          var myLatLng = {lat: 53.2193133, lng: 6.5669632};
-  
-          var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 12,
-            center: myLatLng
-          });
-  
-          var markers = [];
-          for (let i of users) {
-            markers.push(
-              new google.maps.Marker({
-                position: {lat: parseFloat(i.latitude), lng: parseFloat(i.longitude)},
-                map: map,
-                title: i
-                
-                //hier wil ik eigelijk nog de naam van de gebruiker die vertoont wordt als je op de marker klikt. 
-            }));
-          
 
-          }
+      function initMap() {
 
-          // var marker = new google.maps.Marker({
-          //   position: myLatLng,
-          //   map: map,
-          //   title: 'Hello World!'
-          // });
-          // var marker2 = new google.maps.Marker({
-          //   position: {lat: 53.2183133, lng: 6.5669632},
-          //   map: map,
-          //   title: 'Hello World!'
-          // });
-          
-  
-          // var myLatlng = new google.maps.LatLng(53.2193133,6.5669632);
-          
-          // var mapOptions = {
-          //   zoom: 4,
-          //   center: myLatlng
-          // }
-          // // var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  
-          // // var marker = new google.maps.Marker({
-          // //   position: myLatlng,
-          // //   title:"Hello World!"
-          // });
-  }
-  
-   </script>
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 3,
+          center: {lat:53.2193133 , lng: 6.5669632}
+        });
 
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBfM9rq072pO3kYg5hTX_69uA-6LeVKhF8&libraries=places&callback=initMap" async defer></script>
-
-
-</body>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBfM9rq072pO3kYg5hTX_69uA-6LeVKhF8&callback=initMap">
+    </script>
+  </body>
 </html>
+  
+
